cc91d1ec705e3a0985ed8b45fb8b07da381b288f
Laurence Morgan <laurence@gfm.co.uk>
2017-04-07 10:50:55 +0100

    `$0 -c '$CODE'` now returns exit number. eg sh -c './murex -c "false"; $?' returns "false\n1"

--- main.go
+++ main.go
@@ -6,11 +6,17 @@ import (
 	_ "github.com/lmorg/murex/lang/builtins"
 	"github.com/lmorg/murex/lang/types"
 	"github.com/lmorg/murex/shell"
+	"github.com/lmorg/murex/utils"
 	"os"
 	"runtime/trace"
 )
 
 func main() {
+	var (
+		exitNum int
+		err     error
+	)
+
 	readFlags()
 
 	if fTrace != "" {
@@ -29,13 +35,25 @@ func main() {
 
 	switch {
 	case fCommand != "":
-		lang.ProcessNewBlock(
+		exitNum, err = lang.ProcessNewBlock(
 			[]rune(fCommand),
 			nil,
 			nil,
 			nil,
 			types.Null,
 		)
+
+		if err != nil {
+			if exitNum == 0 {
+				exitNum = 1
+			}
+			os.Stderr.WriteString(err.Error() + utils.NewLineString)
+			os.Exit(exitNum)
+		}
+
+		if exitNum != 0 {
+			os.Exit(exitNum)
+		}
 
 	case fStdin:
 		os.Stderr.WriteString("Not implimented yet.\n")
