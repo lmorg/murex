1ddc111b0d71735efe203273665526f26b0ffb61
Laurence Morgan <laurence@gfm.co.uk>
2017-04-07 18:41:05 +0100

    Changed `null` input to generic so it can also be used as `>/dev/null`

--- lang/builtins/typemgmt/types.go
+++ lang/builtins/typemgmt/types.go
@@ -11,7 +11,7 @@ func init() {
 	proc.GoFunctions["pty"] = proc.GoFunction{Func: proc.ExternalPty, TypeIn: types.Null, TypeOut: types.String}
 	proc.GoFunctions["die"] = proc.GoFunction{Func: cmdDie, TypeIn: types.Generic, TypeOut: types.Die}
 	proc.GoFunctions["exit"] = proc.GoFunction{Func: cmdExit, TypeIn: types.Null, TypeOut: types.Null}
-	proc.GoFunctions["null"] = proc.GoFunction{Func: cmdNull, TypeIn: types.Null, TypeOut: types.Null}
+	proc.GoFunctions["null"] = proc.GoFunction{Func: cmdNull, TypeIn: types.Generic, TypeOut: types.Null}
 	proc.GoFunctions["true"] = proc.GoFunction{Func: cmdTrue, TypeIn: types.Null, TypeOut: types.Boolean}
 	proc.GoFunctions["false"] = proc.GoFunction{Func: cmdFalse, TypeIn: types.Null, TypeOut: types.Boolean}
 	proc.GoFunctions["!"] = proc.GoFunction{Func: cmdNot, TypeIn: types.Generic, TypeOut: types.Boolean}
