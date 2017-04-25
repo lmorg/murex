5761e2c4ea4390c30b05bd5b8ef5f6368d52f9f4
Laurence <mail@laurencemorgan.co.uk>
2017-04-25 23:54:28 +0100

    Added eval function via https://github.com/Knetic/govaluate

--- lang/builtins/math/math.go
+++ lang/builtins/math/math.go
@@ -1,32 +1,41 @@
 package math
 
 import (
+	"fmt"
+	gov "github.com/Knetic/govaluate"
 	"github.com/lmorg/murex/lang/proc"
 	"github.com/lmorg/murex/lang/types"
+	"strings"
 )
 
 func init() {
-	proc.GoFunctions["math"] = proc.GoFunction{Func: cmdMath, TypeIn: types.Generic, TypeOut: types.Generic}
+	proc.GoFunctions["eval"] = proc.GoFunction{Func: cmdEval, TypeIn: types.Generic, TypeOut: types.Generic}
 }
 
-func cmdMath(p *proc.Process) (err error) {
-
-	if p.Method == true {
-
-	}
-
-}
-
-func parseFormula(formula []byte) {
-	type fTree struct {
-		symbol string
-	}
-	for i, b := range formula {
-		switch b {
-		case ",":
-			// do nothing
-		case ".":
-		case "/", "*", "-", "+", "(", ")":
+func cmdEval(p *proc.Process) error {
+	defer func() {
+		if r := recover(); r != nil {
+			p.ExitNum = 2
+			msg := fmt.Sprint("Panic caught: ", r)
+			p.Stderr.Writeln([]byte(msg))
 		}
+	}()
+
+	expression, err := gov.NewEvaluableExpression(p.Parameters.AllString())
+	if err != nil {
+		return err
 	}
+	result, err := expression.Evaluate(nil)
+	if err != nil {
+		return err
+	}
+
+	v, err := types.ConvertGoType(result, types.String)
+	if err != nil {
+		return err
+	}
+
+	s := strings.Replace(v.(string), ".000000", "", 1) // TODO: this is hacky!!!
+	_, err = p.Stdout.Write([]byte(s))
+	return err
 }
