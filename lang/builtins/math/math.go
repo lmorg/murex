4702eb90aa4df02c6058e5ed05998807eb246f28
Laurence <mail@laurencemorgan.co.uk>
2017-04-26 00:06:21 +0100

    started work on `let`

--- lang/builtins/math/math.go
+++ lang/builtins/math/math.go
@@ -9,7 +9,8 @@ import (
 )
 
 func init() {
-	proc.GoFunctions["eval"] = proc.GoFunction{Func: cmdEval, TypeIn: types.Generic, TypeOut: types.Generic}
+	proc.GoFunctions["eval"] = proc.GoFunction{Func: cmdEval, TypeIn: types.Null, TypeOut: types.Generic}
+	proc.GoFunctions["let"] = proc.GoFunction{Func: cmdLet, TypeIn: types.Null, TypeOut: types.Boolean}
 }
 
 func cmdEval(p *proc.Process) error {
@@ -30,12 +31,42 @@ func cmdEval(p *proc.Process) error {
 		return err
 	}
 
-	v, err := types.ConvertGoType(result, types.String)
+	cgt, err := types.ConvertGoType(result, types.String)
 	if err != nil {
 		return err
 	}
 
-	s := strings.Replace(v.(string), ".000000", "", 1) // TODO: this is hacky!!!
+	s := strings.Replace(cgt.(string), ".000000", "", 1) // TODO: this is hacky!!!
+	_, err = p.Stdout.Write([]byte(s))
+	return err
+}
+
+func cmdLet(p *proc.Process) error {
+	params := p.Parameters.AllString()
+	// TODO
+	defer func() {
+		if r := recover(); r != nil {
+			p.ExitNum = 2
+			msg := fmt.Sprint("Panic caught: ", r)
+			p.Stderr.Writeln([]byte(msg))
+		}
+	}()
+
+	expression, err := gov.NewEvaluableExpression(params)
+	if err != nil {
+		return err
+	}
+	result, err := expression.Evaluate(nil)
+	if err != nil {
+		return err
+	}
+
+	cgt, err := types.ConvertGoType(result, types.String)
+	if err != nil {
+		return err
+	}
+
+	s := strings.Replace(cgt.(string), ".000000", "", 1) // TODO: this is hacky!!!
 	_, err = p.Stdout.Write([]byte(s))
 	return err
 }
