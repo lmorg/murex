b60ed0efc795bbe151ad421955b49c2cf45222f1
Laurence Morgan <laurence@gfm.co.uk>
2017-04-26 17:33:19 +0100

    update to `let`

--- lang/builtins/math/math.go
+++ lang/builtins/math/math.go
@@ -1,10 +1,12 @@
 package math
 
 import (
+	"errors"
 	"fmt"
 	gov "github.com/Knetic/govaluate"
 	"github.com/lmorg/murex/lang/proc"
 	"github.com/lmorg/murex/lang/types"
+	"regexp"
 	"strings"
 )
 
@@ -13,46 +15,31 @@ func init() {
 	proc.GoFunctions["let"] = proc.GoFunction{Func: cmdLet, TypeIn: types.Null, TypeOut: types.Boolean}
 }
 
-func cmdEval(p *proc.Process) error {
+var rxLet *regexp.Regexp = regexp.MustCompile(`^([-_a-zA-Z0-9]+)\s*=`)
+
+func cmdLet(p *proc.Process) error {
+	params := p.Parameters.AllString()
+
+	if !rxLet.MatchString(params) {
+		return errors.New("Invalid syntax for `let`. Should be `let variable-name = expression`.")
+	}
+
+	params = rxLet.ReplaceAllString(params, "")
+
+	return cmdEval(p)
+}
+
+func cmdEval(p *proc.Process) (err error) {
 	defer func() {
 		if r := recover(); r != nil {
 			p.ExitNum = 2
-			msg := fmt.Sprint("Panic caught: ", r)
-			p.Stderr.Writeln([]byte(msg))
+			err = errors.New(fmt.Sprint("Panic caught: ", r))
+			//msg := fmt.Sprint("Panic caught: ", r)
+			//p.Stderr.Writeln([]byte(msg))
 		}
 	}()
 
 	expression, err := gov.NewEvaluableExpression(p.Parameters.AllString())
-	if err != nil {
-		return err
-	}
-	result, err := expression.Evaluate(nil)
-	if err != nil {
-		return err
-	}
-
-	cgt, err := types.ConvertGoType(result, types.String)
-	if err != nil {
-		return err
-	}
-
-	s := strings.Replace(cgt.(string), ".000000", "", 1) // TODO: this is hacky!!!
-	_, err = p.Stdout.Write([]byte(s))
-	return err
-}
-
-func cmdLet(p *proc.Process) error {
-	params := p.Parameters.AllString()
-	// TODO
-	defer func() {
-		if r := recover(); r != nil {
-			p.ExitNum = 2
-			msg := fmt.Sprint("Panic caught: ", r)
-			p.Stderr.Writeln([]byte(msg))
-		}
-	}()
-
-	expression, err := gov.NewEvaluableExpression(params)
 	if err != nil {
 		return err
 	}
