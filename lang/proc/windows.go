b3d622ae3972d8de79e7e48fad7bb5a1b30990cb
Laurence Morgan <laurence@gfm.co.uk>
2017-05-22 12:01:04 +0100

    Fudging cmd.exe dependence in Windows

--- lang/proc/windows.go
+++ lang/proc/windows.go
@@ -5,7 +5,6 @@ package proc
 // Disable PTY support in Windows.
 func ExternalPty(p *Process) error {
 	// Nasty fudge to work around the fact that a lot of Windows's common commands are built into cmd.exe
-	p.Parameters.SetPrepend("/c")
-	p.Parameters.SetPrepend("cmd")
+	p.Parameters.Params = append([]string{"cmd", "/c"}, p.Parameters.Params...)
 	return External(p)
 }
