2d11000b926c79ea36977840f660355ff78112ce
Laurence Morgan <laurence@gfm.co.uk>
2017-05-22 11:43:30 +0100

    Fixed compile error on Windows

--- lang/proc/windows.go
+++ lang/proc/windows.go
@@ -2,15 +2,6 @@
 
 package proc
 
-import (
-	"github.com/kr/pty"
-	"io"
-	"os"
-	"os/exec"
-	"strconv"
-	"strings"
-)
-
 // Disable PTY support in Windows.
 func ExternalPty(p *Process) error {
 	return External(p)
