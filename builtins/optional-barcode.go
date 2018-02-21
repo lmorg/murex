7358cf2c32db53fbece32797e00b33b1a9ece9c6
Laurence Morgan <laurence.morgan@capablue.com>
2018-02-21 09:06:48 +0000

    New optional: `qr` for generating barcodes

---
+++ builtins/optional-barcode.go
@@ -0,0 +1,4 @@
+package builtins
+
+// This is an optional package that covers some barcode encodigs
+import _ "github.com/lmorg/murex/builtins/optional/qr" // compile optional builtins
---
+++ builtins/optional/qr/init.go
@@ -0,0 +1,37 @@
+package string
+
+import (
+	"github.com/boombuler/barcode"
+	"github.com/boombuler/barcode/qr"
+	"github.com/lmorg/murex/lang/proc"
+	"image/png"
+)
+
+func init() {
+	proc.GoFunctions["qr"] = cmdQr
+}
+
+func cmdQr(p *proc.Process) error {
+	p.Stdout.SetDataType("image")
+
+	b, err := p.Stdin.ReadAll()
+	if err != nil {
+		return err
+	}
+
+	// Create the barcode
+	qrCode, err := qr.Encode(string(b), qr.M, qr.Auto)
+	if err != nil {
+		return err
+	}
+
+	// Scale the barcode to 200x200 pixels
+	qrCode, _ = barcode.Scale(qrCode, 200, 200)
+
+	//// create the output file
+	//file, _ := os.Create("qrcode.png")
+	//defer file.Close()
+
+	// encode the barcode as png
+	return png.Encode(p.Stdout, qrCode)
+}
