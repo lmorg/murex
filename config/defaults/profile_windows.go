448b671f9dbfa08dec0dd69ef12c0354f40afeb4
Laurence <mail@laurencemorgan.co.uk>
2018-05-17 22:53:11 +0100

    Fix bug compiling on Windows

--- config/defaults/profile_windows.go
+++ config/defaults/profile_windows.go
@@ -2,12 +2,11 @@
 
 package defaults
 
-// DefaultMurexProfile is basically just the contents of the example murex_profile but wrapped up in Go code so it can
-// be compiled into the portable executable. This is also done to make things a little more user friendly out of the box
-// ie people don't need to create their own ~/.murex_profile nor `source` the file in /examples.
-const DefaultMurexProfile string = `
+func init() {
+    murexProfile = append(murexProfile, `
 
 autocomplete set go { [{
     "Flags": [ "build", "clean", "doc", "env", "bug", "fix", "fmt", "generate", "get", "install", "list", "run", "test", "tool", "version", "vet", "help" ]
 }] }
-`
+`)
+}
