f436287cc43b17b5cb4d78266824bede24e138bc
Laurence Morgan <laurence.morgan@capablue.com>
2019-02-05 11:55:53 +0000

    Add ignored tests. Will need to rewrite them and remove the compiler ignore flag

---
+++ lang/test_test.go
@@ -0,0 +1,19 @@
+package lang
+
+import (
+	"testing"
+)
+
+// TestMurexTestingFramework tests murex's testing framework using Go's testing
+// framework (confused? Essentially murex shell scripts have a testing framework
+// leveredged via the `test` builtin. This can be used for testing and debugging
+// murex shell script. The concept behind them is that you place all of the test
+// code within the normal shell script and they sit there idle while murex is
+// running. However the moment you enable the testing flag (via `config`) those
+// test builtins start writing their results to the test report for your review)
+//
+// This Go source file tests that murex's test builtins and report functions
+// work by testing the Go code that resides behind them.
+func TestMurexTestingFramework(t *testing.T) {
+
+}
