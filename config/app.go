6e18befb1ceaf12a26014be534e46382fdddba60
Laurence Morgan <laurence.morgan@capablue.com>
2019-03-24 19:26:51 +0000

    Updated man page parsing on OSX

--- config/app.go
+++ config/app.go
@@ -4,4 +4,4 @@ package config
 const AppName = "murex"
 
 // Version number of $SHELL
-const Version = "0.31.0000 BETA"
+const Version = "0.31.0100 BETA"
--- utils/man/man_unix.go
+++ utils/man/man_unix.go
@@ -13,43 +13,17 @@ import (
 )
 
 var (
-	rxMatchManSection   *regexp.Regexp = regexp.MustCompile(`/man[1678]/`)
-	rxMatchFlagsEscaped *regexp.Regexp = regexp.MustCompile(`\\f[BI]((\\-|-)[a-zA-Z0-9]|(\\-\\-|--)[\\\-a-zA-Z0-9]+).*?\\f[RP]`)
-	rxMatchFlagsQuoted  *regexp.Regexp = regexp.MustCompile(`\.IP "(.*?)"`)
-	rxMatchFlagsOther   *regexp.Regexp = regexp.MustCompile(`\.B (.*?)\\fR`)
-	rxMatchFlagsFlag    *regexp.Regexp = regexp.MustCompile(`(--[\-a-zA-Z0-9]+)`)
-	rxReplaceMarkup     *regexp.Regexp = regexp.MustCompile(`\.[a-zA-Z]+(\s|)`)
+	rxMatchManSection   = regexp.MustCompile(`/man[1678]/`)
+	rxMatchFlagsEscaped = regexp.MustCompile(`\\f[BI]((\\-|-)[a-zA-Z0-9]|(\\-\\-|--)[\\\-a-zA-Z0-9]+).*?\\f[RP]`)
+	rxMatchFlagsQuoted  = regexp.MustCompile(`\.IP "(.*?)"`)
+	rxMatchFlagsDarwin  = regexp.MustCompile(`\.It Fl ([a-zA-Z0-9])`)
+	rxMatchFlagsOther   = regexp.MustCompile(`\.B (.*?)`)
+	//rxMatchFlagsOther   = regexp.MustCompile(`\.B (.*?)\\fR`)
+
+	rxMatchGetFlag = regexp.MustCompile(`(--[\-a-zA-Z0-9]+)`)
+
+	rxReplaceMarkup = regexp.MustCompile(`\.[a-zA-Z]+(\s|)`)
 )
-
-/*
-MANUAL SECTIONS (Linux)
-    The standard sections of the manual include:
-
-    1      User Commands
-    2      System Calls
-    3      C Library Functions
-    4      Devices and Special Files
-    5      File Formats and Conventions
-    6      Games et. al.
-    7      Miscellanea
-    8      System Administration tools and Daemons
-
-    Distributions customize the manual section to their specifics,
-    which often include additional sections.
-*/
-/*
-	OpenBSD
-	1	General commands (tools and utilities).
-	2	System calls and error numbers.
-	3	Library functions.
-	3p	perl(1) programmer's reference guide.
-	4	Device drivers.
-	5	File formats.
-	6	Games.
-	7	Miscellaneous information.
-	8	System maintenance and operation commands.
-	9	Kernel internals.
-*/
 
 // GetManPages executes `man -w` to locate the manual files
 func GetManPages(exe string) []string {
@@ -129,7 +103,7 @@ func parseFlags(flags *map[string]bool, filename string) {
 				continue
 			}
 
-			flag := rxMatchFlagsFlag.FindAllStringSubmatch(match[i][1], -1)
+			flag := rxMatchGetFlag.FindAllStringSubmatch(match[i][1], -1)
 			for j := range flag {
 				if len(flag[j]) == 0 {
 					continue
@@ -139,13 +113,25 @@ func parseFlags(flags *map[string]bool, filename string) {
 			}
 		}
 
+		match = rxMatchFlagsDarwin.FindAllStringSubmatch(s, -1) // eg `cat` on OSX
+		for i := range match {
+			if len(match[i]) == 0 {
+				continue
+			}
+
+			(*flags)["-"+match[i][1]] = true
+		}
+
 		match = rxMatchFlagsOther.FindAllStringSubmatch(s, -1)
 		for i := range match {
 			if len(match[i]) == 0 {
 				continue
 			}
 
-			flag := rxMatchFlagsFlag.FindAllStringSubmatch(match[i][1], -1)
+			// Fix \^ seen on some OSX man pages
+			match[i][1] = strings.Replace(match[i][1], `\^`, "", -1)
+
+			flag := rxMatchGetFlag.FindAllStringSubmatch(match[i][1], -1)
 			for j := range flag {
 				if len(flag[j]) == 0 {
 					continue
