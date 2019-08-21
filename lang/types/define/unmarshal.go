caa930df837004fa19defe0d6a7104f55e141202
Laurence Morgan <laurence.morgan@capablue.com>
2019-08-21 07:32:26 +0100

    Add a warning comment about the lack of mutex on (un)marshaldata map

--- lang/types/define/unmarshal.go
+++ lang/types/define/unmarshal.go
@@ -10,7 +10,14 @@ import (
 // murex builtin commands (etc).
 // See docs/apis/marshaldata.md for more details
 func UnmarshalData(p *lang.Process, dataType string) (v interface{}, err error) {
-
+	// This is one of the very few maps in Murex which isn't hidden behind a sync
+	// lock of one description or other. The rational is that even mutexes can
+	// add a noticeable overhead on the performance of tight loops and I expect
+	// this function to be called _a lot_ while also only needing to be written
+	// to via code residing in within builtin types init() function (ie while
+	// murex is effectively single threaded). So there shouldn't be any data-
+	// races -- PROVIDING developers strictly follow the pattern of only writing
+	// to this map within init() func's.
 	if Unmarshallers[dataType] == nil {
 		return nil, errors.New("I don't know how to unmarshal `" + dataType + "`.")
 	}
