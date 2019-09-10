034e384347ec82c59bf1c84f7c5722a06ba60e02
Laurence Morgan <laurence.morgan@capablue.com>
2019-09-10 23:11:18 +0100

    next commit

--- lang/test_units.go
+++ lang/test_units.go
@@ -1,7 +1,10 @@
 package lang
 
 import (
+	"errors"
+	"fmt"
 	"strings"
+	"sync"
 
 	"github.com/lmorg/murex/lang/ref"
 )
@@ -16,16 +19,45 @@ import (
 
 // UnitTests is an exportable class for all things murex unit tests
 type UnitTests struct {
-	units []unitTest
+	units []*unitTest
+	mutex sync.Mutex
 }
 
-// RunTests runs all unit tests against a specific murex function
-func (ut UnitTests) RunTests(name string) {
-	for i := range ut.units {
-		if ut.units[i].Function == name {
-			runTest(&ut.units[i].TestPlan, name)
+type unitTest struct {
+	Function string // if private it should contain path module path
+	FileRef  *ref.File
+	TestPlan *UnitTestPlan
+}
+
+// Add a new unit test
+func (ut *UnitTests) Add(function string, test *UnitTestPlan, fileRef *ref.File) {
+	newUnitTest := &unitTest{
+		Function: function,
+		TestPlan: test,
+		FileRef:  fileRef,
+	}
+
+	ut.mutex.Lock()
+	ut.units = append(ut.units, newUnitTest)
+	ut.mutex.Unlock()
+}
+
+// Run all unit tests against a specific murex function
+func (ut *UnitTests) Run(function string) bool {
+	ut.mutex.Lock()
+	utCopy := make([]*unitTest, len(ut.units))
+	copy(utCopy, ut.units)
+	ut.mutex.Unlock()
+
+	passed := true
+
+	for i := range utCopy {
+		if utCopy[i].Function == function {
+			passed = passed && runTest(utCopy[i].TestPlan, function)
 		}
 	}
+
+	return passed
 }
 
 // UnitTestPlan is defined via JSON and specifies an individual test plan
@@ -39,17 +71,13 @@ type UnitTestPlan struct {
 	PostBlock  string
 }
 
-type unitTest struct {
-	Function string // if private it should contain path module path
-	FileRef  *ref.File
-	TestPlan UnitTestPlan
-}
-
-func runTest(test *UnitTestPlan, name string) {
+func runTest(test *UnitTestPlan, function string) bool {
 	var (
 		preExitNum, testExitNum, postExitNum int
 		preForkErr, testForkErr, postForkErr error
-		F_NO_STDIN                           int
+		F_STDIN                              int
+		passed                               bool
+		stdout, stderr                       string
 	)
 
 	if len(test.Stdin) == 0 {
@@ -59,8 +87,14 @@ func runTest(test *UnitTestPlan, name string) {
 	}
 
 	fork := ShellProcess.Fork(F_STDIN | F_CREATE_STDOUT | F_CREATE_STDERR | F_FUNCTION)
+	fork.Name = function
 	if len(test.Stdin) > 0 {
-		fork.Stdin.Write([]byte(test.Stdin))
+		_, err := fork.Stdin.Write([]byte(test.Stdin))
+		//fork.Stdin.Close()
+		if err != nil {
+			fmt.Println(err)
+			return false
+		}
 	}
 
 	// Run any initializing code...if defined
@@ -68,19 +102,44 @@ func runTest(test *UnitTestPlan, name string) {
 		preExitNum, preForkErr = fork.Execute([]rune(test.PreBlock))
 	}
 
-	testExitNum, testForkErr := runFunction(name, fork)
+	testExitNum, testForkErr = runFunction(function, fork)
 
 	// Run any clear down code...if defined
 	if len(test.PostBlock) > 0 {
 		postExitNum, postForkErr = fork.Execute([]rune(test.PostBlock))
 	}
+
+	b, err := fork.Stdout.ReadAll()
+	if err != nil {
+		fmt.Println(err)
+		return false
+	}
+	stdout = string(b)
+
+	b, err = fork.Stderr.ReadAll()
+	if err != nil {
+		fmt.Println(err)
+		return false
+	}
+	stderr = string(b)
+
+	fmt.Println("unit test:",
+		preExitNum, testExitNum, postExitNum,
+		preForkErr, testForkErr, postForkErr,
+		F_STDIN)
+
+	passed = testExitNum != test.ExitNumber || preExitNum != 0 || postExitNum != 0 ||
+		testForkErr != nil || preForkErr != nil || postForkErr != nil ||
+		stdout != test.Stdout || stderr != test.Stderr
+
+	return passed
 }
 
-func runFunction(name string, fork *Fork) (int, error) {
-	if strings.Contains(name, "/") {
-		// run private
+func runFunction(function string, fork *Fork) (int, error) {
+	if strings.Contains(function, "/") {
+		return 0, errors.New("TODO: support me!")
 	} else {
-		block, err := MxFunctions.Block(name)
+		block, err := MxFunctions.Block(function)
 		if err != nil {
 			return 0, err
 		}
