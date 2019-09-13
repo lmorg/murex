b97d96dcc0493132fd823b1184617e0faffe5e40
Laurence Morgan <lmorg@users.noreply.github.com>
2019-09-13 08:34:46 +0100

    #140 `if` shouldn't return a non-zero exit status (#150)

--- config/app.go
+++ config/app.go
@@ -5,4 +5,4 @@ const AppName = "murex"
 
 // Version number of $SHELL
 // Format of version string should be "(major).(minor).(revision) DESCRIPTION"
-const Version = "0.40.0000 BETA"
+const Version = "0.40.1000 BETA"
