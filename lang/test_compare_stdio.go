b13f51e32ee137bce2a65878b34f4e7cfe37303c
Laurence Morgan <laurence.morgan@capablue.com>
2019-09-18 09:30:38 +0100

    Add two files

---
+++ lang/test_compare_stdio.go
@@ -0,0 +1,72 @@
+package lang
+
+/*
+	This test library relates to the testing framework within the murex
+	language itself rather than Go's test framework within the murex project.
+
+	The naming convention here is basically the inverse of Go's test naming
+	convention. ie Go source files will be named "test_unit.go" (because
+	calling it unit_test.go would mean it's a Go test rather than murex test)
+	and the code is named UnitTestPlans (etc) rather than TestUnitPlans (etc)
+	because the latter might suggest they would be used by `go test`. This
+	naming convention is a little counterintuitive but it at least avoids
+	naming conflicts with `go test`.
+*/
+
+import (
+	"github.com/lmorg/murex/lang/proc/stdio"
+)
+
+func testBlock(test *TestProperties, p *Process, block []rune, stdin []byte, dt string, property string, failed *bool) {
+	fork := p.Fork(F_CREATE_STDIN | F_CREATE_STDERR | F_CREATE_STDOUT)
+
+	fork.Stdin.SetDataType(dt)
+	_, err := fork.Stdin.Write(stdin)
+	if err != nil {
+		p.Tests.AddResult(test, p, TestFailed, tMsgWriteErr(property, err))
+		*failed = true
+		return
+	}
+
+	exitNum, err := fork.Execute(block)
+	if err != nil {
+		p.Tests.AddResult(test, p, TestFailed, tMsgCompileErr(property, err))
+		return
+	}
+
+	if exitNum == 0 {
+		p.Tests.AddResult(test, p, TestInfo, tMsgExitNumZero(property))
+	} else {
+		p.Tests.AddResult(test, p, TestFailed, tMsgExitNumNotZero(property, exitNum))
+		*failed = true
+	}
+
+	testReadAllOut(test, p, fork.Stdout, property, failed)
+	testReadAllErr(test, p, fork.Stderr, property, failed)
+}
+
+func testReadAllOut(test *TestProperties, p *Process, std stdio.Io, property string, failed *bool) {
+	b, err := std.ReadAll()
+	if err != nil {
+		p.Tests.AddResult(test, p, TestFailed, tMsgReadErr("stdout", property, err))
+		*failed = true
+	}
+
+	if len(b) != 0 {
+		p.Tests.AddResult(test, p, TestInfo, tMsgStdout(property, b))
+		*failed = true
+	}
+}
+
+func testReadAllErr(test *TestProperties, p *Process, std stdio.Io, property string, failed *bool) {
+	b, err := std.ReadAll()
+	if err != nil {
+		p.Tests.AddResult(test, p, TestFailed, tMsgReadErr("stderr", property, err))
+		*failed = true
+	}
+
+	if len(b) != 0 {
+		p.Tests.AddResult(test, p, TestFailed, tMsgStderr(property, b))
+		*failed = true
+	}
+}
---
+++ lang/test_messages.go
@@ -0,0 +1,67 @@
+package lang
+
+/*
+	This test library relates to the testing framework within the murex
+	language itself rather than Go's test framework within the murex project.
+
+	The naming convention here is basically the inverse of Go's test naming
+	convention. ie Go source files will be named "test_unit.go" (because
+	calling it unit_test.go would mean it's a Go test rather than murex test)
+	and the code is named UnitTestPlans (etc) rather than TestUnitPlans (etc)
+	because the latter might suggest they would be used by `go test`. This
+	naming convention is a little counterintuitive but it at least avoids
+	naming conflicts with `go test`.
+*/
+
+import "fmt"
+
+func tMsgPassed() string { return "All test conditions were met" }
+func tMsgStdout(property string, stdout []byte) string {
+	return fmt.Sprintf("%s output: %s", property, stdout)
+}
+
+func tMsgStderr(property string, stdout []byte) string {
+	return fmt.Sprintf("%s returned an error: %s", property, stdout)
+}
+
+func tMsgReadErr(stdType string, property string, err error) string {
+	return fmt.Sprintf("Error reading %s from %s: %s", stdType, property, err)
+}
+func tMsgWriteErr(property string, err error) string {
+	return fmt.Sprintf("Error writing to stdin for %s: %s", property, err)
+}
+
+func tMsgCompileErr(property string, err error) string {
+	return fmt.Sprintf("%s failed to compile: %s", property, err)
+}
+func tMsgNoneZeroExit(property string, exitnum int) string {
+	return fmt.Sprintf("%s exit num non-zero: %d", property, exitnum)
+}
+
+func tMsgExitNumMismatch(exp int, act int) string {
+	return fmt.Sprintf("ExitNum mismatch. Exp: %d, act: %d", exp, act)
+}
+func tMsgExitNumMatch() string {
+	return fmt.Sprintf("ExitNum matches expected")
+}
+
+func tMsgExitNumNotZero(property string, exitnum int) string {
+	return fmt.Sprintf("%s failed the test with an exit num of %d", property, exitnum)
+}
+func tMsgExitNumZero(property string) string {
+	return fmt.Sprintf("%s passed the test. Returned true", property)
+}
+
+func tMsgStringMismatch(property, std []byte) string {
+	return fmt.Sprintf("%s string mismatch. act: '%s'", property, std)
+}
+func tMsgStringMatch(property string) string {
+	return fmt.Sprintf("%s matches expected string", property)
+}
+
+func tMsgRegexMismatch(property string, std []byte) string {
+	return fmt.Sprintf("%s expression did not match. act: '%s'", property, std)
+}
+func tMsgRegexMatch(property string) string {
+	return fmt.Sprintf("%s matches expected regex expression", property)
+}
