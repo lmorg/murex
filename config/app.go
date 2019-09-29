eaaaffa5fd5781f8c7b0d9aaa091411c1a77f51f
Laurence <mail@laurencemorgan.co.uk>
2019-09-29 17:23:40 +0100

    Updates to CI

--- config/app.go
+++ config/app.go
@@ -5,4 +5,4 @@ const AppName = "murex"
 
 // Version number of $SHELL
 // Format of version string should be "(major).(minor).(revision) DESCRIPTION"
-const Version = "0.42.1240 BETA"
+const Version = "0.42.2000 BETA"
--- test/ci.sh
+++ test/ci.sh
@@ -1,25 +1,39 @@
-#!/bin/sh
+#!/bin/bash
+
+set -e
 
 test/pre-commit
 
 echo "Starting count server...."
-go run github.com/lmorg/murex/test/count/server &
 export MUREX_TEST_COUNT=http
-
+go run github.com/lmorg/murex/test/count/server 2>/dev/null &
+sleep 1
+        
 echo "Run golang unit tests...."
 go test ./... -count 10 -race -coverprofile=coverage.txt -covermode=atomic
-export MUREXTESTS=$(curl http://localhost:38000/t)
-export MUREXVERSION="$(./murex -c 'version --no-app-name')"
+export MUREXTESTS="$(curl -s http://localhost:38000/t)"
 
-echo "Run murex shell script unit tests..."
+echo "Compiling murex...."
+go install github.com/lmorg/murex
+
+echo "Run murex shell script unit tests...."
 murex --run-tests
 
 echo "Building latest binaries...."
-./murex ./test/build_all_platforms.mx --no-colour --inc-latest --compress
+murex ./test/build_all_platforms.mx --no-colour --inc-latest --compress
 
 echo "Building website...."
 mv -v ./bin ./docs/
-sed -i "s/\$DATE/`date`/; s/\$COMMITHASHSHORT/`git rev-parse HEAD | cut -c1-7`/; s/\$COMMITHASHLONG/`git rev-parse HEAD`/; s/\$MUREXVERSION/$MUREXVERSION/; s/\$MUREXTESTS/$MUREXTESTS/" gen/website/footer.html
+export MUREXVERSION="$(murex -c 'version --no-app-name')"
+export MUREXCOMMITS="$(git rev-parse HEAD)"
+export MUREXCOMMITL="$(git rev-parse HEAD)"
+
+sed -i "s/\$DATE/`date`/;
+        s/\$COMMITHASHSHORT/$MUREXCOMMITS/;
+        s/\$COMMITHASHLONG/$MUREXCOMMITL/;
+        s/\$MUREXVERSION/$MUREXVERSION/;
+        s/\$MUREXTESTS/$MUREXTESTS/" \
+        gen/website/footer.html
 mv -v *.md ./docs/
 mv -v gen/website/*.css ./docs/
 find docs -name "*.md" -exec gen/website/find-exec.sh {} \;
