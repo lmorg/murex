e12146886dfab11e2bbec114cf3ba46bd6b77270
Laurence Morgan <laurence.morgan@capablue.com>
2019-10-14 22:21:16 +0100

    Reorder TokenType

--- lang/proc/parameters/tokens.go
+++ lang/proc/parameters/tokens.go
@@ -17,11 +17,11 @@ const (
 	// TokenTypeNil: no parameter set or uninitialised parameter.
 	TokenTypeNil = iota
 
-	// TokenTypeValue: key is not a variable. eg `command "just a parameter"`
-	TokenTypeValue
-
 	// TokenTypeNamedPipe: key is a named pipe. eg command <stdio>
 	TokenTypeNamedPipe
+
+	// TokenTypeValue: key is not a variable. eg `command "just a parameter"`
+	TokenTypeValue
 
 	// TokenTypeString: key is a variable. Expand as a single string. eg `command $variable`
 	TokenTypeString
