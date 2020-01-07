9610bac17ae487b35c7e3787ab00825f0b0a397f
Laurence Morgan <laurence.morgan@capablue.com>
2020-01-07 23:24:29 +0000

    Autocomplete suggestions for variables

--- builtins/core/runtime/runtime_test.go
+++ builtins/core/runtime/runtime_test.go
@@ -1,9 +1,11 @@
 package cmdruntime
 
 import (
+	"os"
 	"testing"
 
 	"github.com/lmorg/murex/test"
+	"github.com/lmorg/murex/test/count"
 	"github.com/lmorg/murex/utils/json"
 )
 
@@ -26,4 +28,44 @@ func marshalHelp(t *testing.T) string {
 		t.Errorf("Cannot marshal help(): %s", err)
 	}
 	return string(b)
+}
+
+func TestDumpExports(t *testing.T) {
+	namespace := "MUREX_TEST_TestDumpExports_"
+
+	tests := map[string]string{
+		"nil":       "",
+		"text":      "testing123",
+		"s p a c e": "testing 123",
+		"equals":    "testing=123",
+	}
+
+	count.Tests(t, len(tests))
+
+	for k, v := range tests {
+		if err := os.Setenv(namespace+k, v); err != nil {
+			t.Error("Unable to set env var for test!")
+			t.Logf("  Name:  '%s'", namespace+k)
+			t.Logf("  Value: '%s'", v)
+			t.Logf("  Error:  %s", err)
+		}
+	}
+
+	m, err := dumpExports()
+	if err != nil {
+		t.Error(err)
+	}
+
+	for k, v := range tests {
+		if m[namespace+k] != v {
+			t.Error("Unexpected map value:")
+			t.Logf("  Name:     '%s'", namespace+k)
+			t.Logf("  Expected: '%s'", v)
+			t.Logf("  Actual:   '%s'", m[namespace+k])
+		}
+
+		if err := os.Unsetenv(namespace + k); err != nil {
+			t.Errorf("Unable to unset '%s': %s", namespace+k, err)
+		}
+	}
 }
