a85a2d3b4c81d675b023f1f4f9493f39a8bf35d7
Laurence <mail@laurencemorgan.co.uk>
2020-01-26 19:32:13 +0000

    Up version number

--- config/app.go
+++ config/app.go
@@ -5,4 +5,4 @@ const AppName = "murex"
 
 // Version number of $SHELL
 // Format of version string should be "(major).(minor).(revision) DESCRIPTION"
-const Version = "0.46.2010 BETA"
+const Version = "0.46.2110 BETA"
