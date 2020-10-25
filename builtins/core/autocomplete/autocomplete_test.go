1a8ca13a99e9e8ed8cfd336c07e944b8c8865306
Laurence Morgan <laurence.morgan@capablue.com>
2020-10-25 17:16:21 +0000

    Add tests for autocomplete and support for `autocomplete get cmd

---
+++ builtins/core/autocomplete/autocomplete_test.go
@@ -0,0 +1,75 @@
+package cmdautocomplete_test
+
+import (
+	"testing"
+
+	_ "github.com/lmorg/murex/builtins"
+	"github.com/lmorg/murex/lang"
+)
+
+func TestAutocomplete(t *testing.T) {
+	block := []rune(
+		`autocomplete set foobar { [{
+			"AnyValue": true,
+			"AllowMultiple": true,
+			"ExecCmdline": true,
+			"Flags": [ "hello", "world" ]
+		}] }
+		
+		autocomplete get foobar -> pretty`)
+
+	expected := `[
+    {
+        "IncFiles": false,
+        "IncDirs": false,
+        "IncExePath": false,
+        "Flags": [
+            "hello",
+            "world"
+        ],
+        "FlagsDesc": null,
+        "Dynamic": "",
+        "DynamicDesc": "",
+        "ListView": false,
+        "FlagValues": null,
+        "Optional": false,
+        "AllowMultiple": true,
+        "Alias": "",
+        "NestedCommand": false,
+        "AnyValue": true,
+        "AutoBranch": false,
+        "ExecCmdline": true
+    }
+]
+`
+
+	lang.InitEnv()
+
+	fork := lang.ShellProcess.Fork(lang.F_NO_STDIN | lang.F_CREATE_STDOUT | lang.F_CREATE_STDERR)
+	exitNum, err := fork.Execute(block)
+	if exitNum != 0 {
+		t.Errorf("None zero exit number: %d", exitNum)
+	}
+	if err != nil {
+		t.Errorf("Error in fork.Execute(): %s", err.Error())
+	}
+
+	stdout, err := fork.Stdout.ReadAll()
+	if err != nil {
+		t.Errorf("Error in fork.Stdout.ReadAll(): %s", err.Error())
+	}
+
+	stderr, err := fork.Stderr.ReadAll()
+	if err != nil {
+		t.Errorf("Error in fork.Stderr.ReadAll(): %s", err.Error())
+	}
+	if len(stderr) > 0 {
+		t.Errorf("Stderr should be empty: %s", string(stderr))
+	}
+
+	if string(stdout) != expected {
+		t.Error("Stdout doesn't match expected:")
+		t.Logf("  Expected: %s", expected)
+		t.Logf("  Actual: %s", string(stdout))
+	}
+}
