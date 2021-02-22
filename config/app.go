28eea7109b6d9861a1bc734a0fc1f70e44edade5
Laurence Morgan <laurence.morgan@capablue.com>
2021-02-22 08:35:47 +0000

    Up version number

--- config/app.go
+++ config/app.go
@@ -5,4 +5,4 @@ const AppName = "murex"
 
 // Version number of $SHELL
 // Format of version string should be "(major).(minor).(revision) DESCRIPTION"
-const Version = "1.6.4100 BETA"
+const Version = "1.6.5000 BETA"
