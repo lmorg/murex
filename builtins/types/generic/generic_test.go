382b9ef3f37e5ba630fa10ac3f3ed1f409c5af2c
Laurence Morgan <laurence.morgan@capablue.com>
2021-03-02 21:29:18 +0000

    #282 Fix off-by-one error in ittIndex() byColumnName

--- builtins/types/generic/generic_test.go
+++ builtins/types/generic/generic_test.go
@@ -1,4 +1,4 @@
-package generic
+package generic_test
 
 import (
 	"testing"
---
+++ builtins/types/generic/index_test.go
@@ -0,0 +1,84 @@
+package generic_test
+
+import (
+	"testing"
+
+	_ "github.com/lmorg/murex/builtins"
+	"github.com/lmorg/murex/test"
+)
+
+func TestGenericIndex(t *testing.T) {
+	tests := []test.MurexTest{
+		{
+			Block:  `a: [0..4] -> cast * -> [0]`,
+			Stdout: "0\n1\n2\n3\n4\n",
+		},
+		{
+			Block:  `a: [0..4] -> cast * -> [:0]`,
+			Stdout: "0\n1\n2\n3\n4\n",
+		},
+		{
+			Block:  `a: [0..4] -> cast * -> [0:]`,
+			Stdout: "0\n",
+		},
+		///
+		{
+			Block:  `a: [0..4] -> cast generic -> [0]`,
+			Stdout: "0\n1\n2\n3\n4\n",
+		},
+		{
+			Block:  `a: [0..4] -> cast generic -> [:0]`,
+			Stdout: "0\n1\n2\n3\n4\n",
+		},
+		{
+			Block:  `a: [0..4] -> cast generic -> [0:]`,
+			Stdout: "0\n",
+		},
+
+		//////
+
+		{
+			Block:  `a: [Mon..Fri] -> cast * -> [Mon]`,
+			Stdout: "Mon\nTue\nWed\nThu\nFri\n",
+		},
+		{
+			Block:   `a: [Mon..Fri] -> cast * -> [:Mon]`,
+			ExitNum: 1,
+		},
+		{
+			Block:  `a: [Mon..Fri] -> cast * -> [:0]`,
+			Stdout: "Mon\nTue\nWed\nThu\nFri\n",
+		},
+		{
+			Block:   `a: [Mon..Fri] -> cast * -> [Mon:]`,
+			ExitNum: 1,
+		},
+		{
+			Block:  `a: [Mon..Fri] -> cast * -> [0:]`,
+			Stdout: "Mon\n",
+		},
+		///
+		{
+			Block:  `a: [Mon..Fri] -> cast generic -> [Mon]`,
+			Stdout: "Mon\nTue\nWed\nThu\nFri\n",
+		},
+		{
+			Block:   `a: [Mon..Fri] -> cast generic -> [:Mon]`,
+			ExitNum: 1,
+		},
+		{
+			Block:  `a: [Mon..Fri] -> cast generic -> [:0]`,
+			Stdout: "Mon\nTue\nWed\nThu\nFri\n",
+		},
+		{
+			Block:   `a: [Mon..Fri] -> cast generic -> [Mon:]`,
+			ExitNum: 1,
+		},
+		{
+			Block:  `a: [Mon..Fri] -> cast generic -> [0:]`,
+			Stdout: "Mon\n",
+		},
+	}
+
+	test.RunMurexTests(tests, t)
+}
