f5b4160295d18cef3a2c45587872677af2e64b3c
Laurence Morgan <laurence.morgan@capablue.com>
2021-05-03 22:30:37 +0100

    website: update header.html + updated ci/cd process

--- test/ci-website.sh
+++ test/ci-website.sh
@@ -1,40 +1,40 @@
 #!/bin/bash
 
-[ -z "$MUREX_BUILD_BIN" ]     && \
-        export MUREX_BUILD_BIN="$(git describe --tags --match bin)"
-[ -z "$MUREX_BUILD_WEBSITE" ] && \
-        export MUREX_BUILD_WEBSITE="$(git describe --tags --match website)"
+#[ -z "$MUREX_BUILD_WEBSITE_ONLY" ] && \
+#        export MUREX_BUILD_WEBSITE_ONLY="$(git describe --tags --match website)"
+
+export MUREX_BUILD="$(git log -1 --pretty=%B | egrep '^website: ')"
 
 mkdir -p /website
 
 
 echo "Building latest binaries...."
-#if [ "$MUREX_BUILD_BIN" == "bin" ]; then
+if [ "$MUREX_BUILD" != "website: " ]; then
         murex ./test/build_all_platforms.mx $MUREX_BUILD_FLAGS
         mv ./bin /website/
-#fi
+else
+        echo "!!! Commit appears to only update website, skipping compile tests !!!"
+fi
 
 echo "Building website...."
-#if [ "$MUREX_BUILD_WEBSITE" == "website" ]; then
-        export MUREXVERSION="$(murex -c 'version --no-app-name')"
-        export MUREXCOMMITS="$(git rev-parse HEAD | cut -c1-7)"
-        export MUREXCOMMITL="$(git rev-parse HEAD)"
-        export MUREXTESTS="$(cat ./murex-test-count.txt)"
+export MUREXVERSION="$(murex -c 'version --no-app-name')"
+export MUREXCOMMITS="$(git rev-parse HEAD | cut -c1-7)"
+export MUREXCOMMITL="$(git rev-parse HEAD)"
+export MUREXTESTS="$(cat ./murex-test-count.txt)"
 
-        sed -i "s/\$DATE/`date`/;
-                s/\$COMMITHASHSHORT/$MUREXCOMMITS/;
-                s/\$COMMITHASHLONG/$MUREXCOMMITL/;
-                s/\$MUREXVERSION/$MUREXVERSION/;
-                s/\$MUREXTESTS/$MUREXTESTS/" \
-                gen/website/footer.html
+sed -i "s/\$DATE/`date`/;
+        s/\$COMMITHASHSHORT/$MUREXCOMMITS/;
+        s/\$COMMITHASHLONG/$MUREXCOMMITL/;
+        s/\$MUREXVERSION/$MUREXVERSION/;
+        s/\$MUREXTESTS/$MUREXTESTS/" \
+        gen/website/footer.html
 
-        cp gen/website/404.md .
-        for f in *.md; do
-                gen/website/find-exec.sh $f
-        done
-        find docs -name "*.md" -exec gen/website/find-exec.sh {} \;
+cp gen/website/404.md .
+for f in *.md; do
+        gen/website/find-exec.sh $f
+done
+find docs -name "*.md" -exec gen/website/find-exec.sh {} \;
 
-        mv *.html gen/website/assets/* ./docs /website/
-#fi
+mv *.html gen/website/assets/* ./docs /website/
 
 echo "Fin!"
