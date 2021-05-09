c1ea72682ef5df33459c25cefd288be95d5bf1b6
Laurence Morgan <laurence.morgan@capablue.com>
2021-05-09 09:31:56 +0100

    wasm: fix async issues wrt Go's http user agent

--- main_js.go
+++ main_js.go
@@ -17,9 +17,9 @@ import (
 const interactive = true
 
 func main() {
-	js.Global().Set("shellExec", js.FuncOf(jsShellExec))
-
 	startMurex()
+
+	js.Global().Set("wasmShellExec", wasmShellExec())
 
 	wait := make(chan bool)
 	<-wait
@@ -43,17 +43,39 @@ func startMurex() {
 	//shell.Start()
 }
 
-func jsShellExec(this js.Value, args []js.Value) interface{} {
-	if len(args) != 1 {
-		return "Invalid number of args passed"
-	}
+// wasmShellExec returns a Promise
+func wasmShellExec() js.Func {
+	return js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+		block := args[0].String()
 
-	block := args[0].String()
+		// Handler for the Promise: this is a JS function
+		// It receives two arguments, which are JS functions themselves: resolve and reject
+		handler := js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+			resolve := args[0]
+			reject := args[1]
 
-	fork := lang.ShellProcess.Fork(lang.F_PARENT_VARTABLE | lang.F_NEW_MODULE | lang.F_NO_STDIN)
-	fork.FileRef.Source.Module = app.Name
-	fork.Stderr = term.NewErr(ansi.IsAllowed())
-	lang.ShellExitNum, _ = fork.Execute([]rune(block))
+			// Now that we have a way to return the response to JS, spawn a goroutine
+			// This way, we don't block the event loop and avoid a deadlock
+			go func() {
+				fork := lang.ShellProcess.Fork(lang.F_PARENT_VARTABLE | lang.F_NEW_MODULE | lang.F_NO_STDIN)
+				fork.FileRef.Source.Module = app.Name
+				fork.Stderr = term.NewErr(ansi.IsAllowed())
+				var err error
+				lang.ShellExitNum, err = fork.Execute([]rune(block))
+				if err != nil {
+					errorConstructor := js.Global().Get("Error")
+					errorObject := errorConstructor.New(err.Error())
+					reject.Invoke(errorObject)
+				}
+				resolve.Invoke("wasmShellExec(): " + block)
+			}()
 
-	return nil
+			// The handler of a Promise doesn't return any value
+			return nil
+		})
+
+		// Create and return the Promise object
+		promiseConstructor := js.Global().Get("Promise")
+		return promiseConstructor.New(handler)
+	})
 }
