ed1e5936f10eb55ba1db4b62f42e1d981a783051
Laurence Morgan <laurence.morgan@capablue.com>
2021-05-24 23:31:10 +0100

    Add tests for `for`, `foreach` & `function`

---
+++ builtins/core/structs/foreach_test.go
@@ -0,0 +1,48 @@
+package structs_test
+
+import (
+	"testing"
+
+	_ "github.com/lmorg/murex/builtins"
+	"github.com/lmorg/murex/lang/types"
+	"github.com/lmorg/murex/test"
+)
+
+func TestForEach(t *testing.T) {
+	tests := []test.MurexTest{
+		{
+			Block:  `a: [Monday..Friday] -> foreach day { out "$day is the best day" }`,
+			Stdout: "Monday is the best day\nTuesday is the best day\nWednesday is the best day\nThursday is the best day\nFriday is the best day\n",
+		},
+		{
+			Block:  `a: [Monday..Friday] -> foreach day { out "$day is the best day" }`,
+			Stdout: "Monday is the best day\nTuesday is the best day\nWednesday is the best day\nThursday is the best day\nFriday is the best day\n",
+		},
+		{
+			Block:  `a: [Mon..Fri] -> foreach { -> suffix "day" }`,
+			Stdout: "Monday\nTueday\nWedday\nThuday\nFriday\n",
+		},
+		{
+			Block:  `ja: [Mon..Fri] -> foreach { -> suffix "day" }`,
+			Stdout: "Monday\nTueday\nWedday\nThuday\nFriday\n",
+		},
+		{
+			Block:  `a: [Mon..Fri] -> foreach --jmap day { $day } { out $day"day" }`,
+			Stdout: `{"Fri":"Friday","Mon":"Monday","Thu":"Thuday","Tue":"Tueday","Wed":"Wedday"}`,
+		},
+		{
+			Block:  `ja: [Mon..Fri] -> foreach --jmap day { $day } { out $day"day" }`,
+			Stdout: `{"Fri":"Friday","Mon":"Monday","Thu":"Thuday","Tue":"Tueday","Wed":"Wedday"}`,
+		},
+		{
+			Block:  `a: [Mon..Fri] -> foreach { null } -> debug -> [[ /Data-Type/Murex  ]]`,
+			Stdout: types.String,
+		},
+		{
+			Block:  `ja: [Mon..Fri] -> foreach { null } -> debug -> [[ /Data-Type/Murex  ]]`,
+			Stdout: types.JsonLines,
+		},
+	}
+
+	test.RunMurexTests(tests, t)
+}
---
+++ builtins/core/structs/function_test.go
@@ -0,0 +1,114 @@
+package structs
+
+import (
+	"testing"
+
+	"github.com/lmorg/murex/lang"
+	"github.com/lmorg/murex/lang/ref"
+	"github.com/lmorg/murex/test/count"
+)
+
+func TestAliases(t *testing.T) {
+	alias := "TestAlias"
+
+	count.Tests(t, 3)
+
+	lang.InitEnv()
+
+	if lang.GlobalAliases.Exists(alias) {
+		t.Fatalf("Expecting '%s' not to exist, yet it does", alias)
+	}
+
+	p := lang.NewTestProcess()
+	p.Name = "alias"
+	p.Parameters.Params = []string{alias + "=foobar"}
+	err := cmdAlias(p)
+	if err != nil {
+		t.Fatalf("Error calling cmdAlias(): %s", err.Error())
+	}
+
+	if !lang.GlobalAliases.Exists(alias) {
+		t.Fatalf("Expecting '%s' to be created, it did not", alias)
+	}
+
+	p = lang.NewTestProcess()
+	p.Name = "!alias"
+	p.IsNot = true
+	p.Parameters.Params = []string{alias}
+	err = cmdUnalias(p)
+	if err != nil {
+		t.Fatalf("Error calling cmdAlias() for the 2nd time: %s", err.Error())
+	}
+
+	if lang.GlobalAliases.Exists(alias) {
+		t.Fatalf("Expecting '%s' to be destroyed, it still exists", alias)
+	}
+}
+
+func TestFunction(t *testing.T) {
+	fn := "TestFunction"
+
+	count.Tests(t, 3)
+
+	lang.InitEnv()
+
+	if lang.MxFunctions.Exists(fn) {
+		t.Fatalf("Expecting '%s' not to exist, yet it does", fn)
+	}
+
+	p := lang.NewTestProcess()
+	p.Name = "function"
+	p.Parameters.Params = []string{fn, "{ test }"}
+	err := cmdFunc(p)
+	if err != nil {
+		t.Fatalf("Error calling cmdFunc(): %s", err.Error())
+	}
+
+	if !lang.MxFunctions.Exists(fn) {
+		t.Fatalf("Expecting '%s' to be created, it did not", fn)
+	}
+
+	p = lang.NewTestProcess()
+	p.Name = "!function"
+	p.IsNot = true
+	p.Parameters.Params = []string{fn}
+	err = cmdUnfunc(p)
+	if err != nil {
+		t.Fatalf("Error calling cmdFunc() for the 2nd time: %s", err.Error())
+	}
+
+	if lang.MxFunctions.Exists(fn) {
+		t.Fatalf("Expecting '%s' to be destroyed, it still exists", fn)
+	}
+}
+
+func TestPrivate(t *testing.T) {
+	fn := "TestPrivate"
+	mod := "GoTest"
+
+	count.Tests(t, 2)
+
+	lang.InitEnv()
+
+	if lang.PrivateFunctions.Exists(fn, mod) {
+		t.Fatalf("Expecting '%s/%s' not to exist, yet it does", mod, fn)
+	}
+
+	p := lang.NewTestProcess()
+	p.Name = "function"
+	p.FileRef = &ref.File{
+		Source: &ref.Source{
+			Module: mod,
+		},
+	}
+
+	p.Parameters.Params = []string{fn, "{ test }"}
+	err := cmdPrivate(p)
+	if err != nil {
+		t.Fatalf("Error calling cmdPrivate(): %s", err.Error())
+	}
+
+	if !lang.PrivateFunctions.Exists(fn, mod) {
+		t.Fatalf("Expecting '%s/%s' to be created, it did not", mod, fn)
+	}
+}
