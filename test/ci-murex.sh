f4de5f42725b6f0d5fc3c48dbc5bc63024201353
Laurence Morgan <laurence.morgan@capablue.com>
2021-10-02 14:28:32 +0100

    Remove stringer from website build

--- test/ci-murex.sh
+++ test/ci-murex.sh
@@ -4,11 +4,11 @@ set -ev
 
 . /etc/ci-murex.env
 
-echo "Compiling stringer...."
-go build -o /bin/stringer golang.org/x/tools/cmd/stringer 
-
-echo "Updating auto-generated code...."
-go generate ./...
+#echo "Compiling stringer...."
+#go build -o /bin/stringer golang.org/x/tools/cmd/stringer 
+#
+#echo "Updating auto-generated code...."
+#go generate ./...
 
 echo "Compiling docgen...."
 go install github.com/lmorg/murex/utils/docgen
