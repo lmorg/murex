465c2b48cbb871c7c57af4a29ba1b565e32b6fb5
Laurence Morgan <laurence.morgan@capablue.com>
2021-11-20 16:14:06 +0000

    Fix failing tests due to changes in APIs due to #380

--- lang/variables.go
+++ lang/variables.go
@@ -154,7 +154,7 @@ func (v *Variables) GetString(name string) (string, error) {
 	// variable not found so lets fallback to the environmental variables
 	s, exists = os.LookupEnv(name)
 
-	if v, _ := v.process.Config.Get("proc", "strict-vars", "bool"); v.(bool) == true && !exists {
+	if v, err := v.process.Config.Get("proc", "strict-vars", "bool"); err == nil && v.(bool) == true && !exists {
 		return "", errVarNotExist
 	}
 
