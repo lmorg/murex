33a347f18e61464098ccc2a3efb66c66a5d7231a
Laurence Morgan <mail@laurencemorgan.co.uk>
2022-03-20 23:24:03 +0000

    more support for updating ANSI title

--- shell/prompt.go
+++ shell/prompt.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"fmt"
 
+	"github.com/lmorg/murex/app"
 	"github.com/lmorg/murex/lang"
 	"github.com/lmorg/murex/lang/types"
 	"github.com/lmorg/murex/utils"
@@ -81,7 +82,10 @@ func writeTitlebar() {
 
 	if exitNum != 0 || err != nil || len(b) == 0 || err2 != nil {
 		lang.ShellProcess.Stderr.Writeln([]byte("Invalid titlebar-func. Block returned false."))
-		return
+		ansititle.Write([]byte(app.Name))
+		if tmux, _ := lang.ShellProcess.Config.Get("proc", "echo-tmux", types.Boolean); tmux.(bool) {
+			ansititle.Tmux([]byte(app.Name))
+		}
 	}
 
 	b = bytes.ReplaceAll(b, []byte{'\r'}, nil)
@@ -93,6 +97,9 @@ func writeTitlebar() {
 	}
 
 	ansititle.Write(b)
+	if tmux, _ := lang.ShellProcess.Config.Get("proc", "echo-tmux", types.Boolean); tmux.(bool) {
+		ansititle.Tmux([]byte(b))
+	}
 }
 
 // ConfigReadGetCursorPos is a dynamic config wrapper function for Prompt.EnableGetCursorPos
@@ -102,12 +109,12 @@ func ConfigReadGetCursorPos() (interface{}, error) {
 
 // ConfigWriteGetCursorPos is a dynamic config wrapper function for Prompt.EnableGetCursorPos
 func ConfigWriteGetCursorPos(v interface{}) error {
-	switch v.(type) {
+	switch v := v.(type) {
 	case bool:
-		Prompt.EnableGetCursorPos = v.(bool)
+		Prompt.EnableGetCursorPos = v
 
 	case string:
-		switch v.(string) {
+		switch v {
 		case types.TrueString:
 			Prompt.EnableGetCursorPos = true
 
@@ -115,11 +122,11 @@ func ConfigWriteGetCursorPos(v interface{}) error {
 			Prompt.EnableGetCursorPos = false
 
 		default:
-			return fmt.Errorf("Expecting 'true' or 'false'. Instead received '%s'", v.(string))
+			return fmt.Errorf("expecting 'true' or 'false'. Instead received '%s'", v)
 		}
 
 	default:
-		return fmt.Errorf("Expecting boolean value. Instead received %T", v)
+		return fmt.Errorf("expecting boolean value. Instead received %T", v)
 	}
 
 	return nil
