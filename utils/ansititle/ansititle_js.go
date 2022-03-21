b285b6cedc3be125631d048f04c3861e9fbd2578
Laurence Morgan <mail@laurencemorgan.co.uk>
2022-03-21 12:17:39 +0000

    ansititle pulling in unused dependencies for WASM

--- utils/ansititle/ansititle_js.go
+++ utils/ansititle/ansititle_js.go
@@ -3,13 +3,6 @@
 
 package ansititle
 
-import (
-	"errors"
-	"os"
-
-	"github.com/lmorg/murex/utils/readline"
-)
-
 func Write(title []byte) error { return nil }
 func Icon(title []byte) error  { return nil }
 func Tmux(title []byte) error  { return nil }
