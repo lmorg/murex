569d6247cf78367a62e18279a9423134c62ac7e6
Laurence Morgan <mail@laurencemorgan.co.uk>
2022-05-26 08:21:15 +0100

    Only run tests on master if version bumped

--- test/ci-murex.sh
+++ test/ci-murex.sh
@@ -4,20 +4,27 @@ set -ev
 
 . /etc/ci-murex.env
 
-#echo "Compiling stringer...."
-#go build -o /bin/stringer golang.org/x/tools/cmd/stringer 
-#
-#echo "Updating auto-generated code...."
-#go generate ./...
+export MUREXVERSION="$(murex -c 'version --no-app-name')"
+OLDVER="$(curl -s https://murex.rocks/VERSION | head -n1)"
 
-echo "Compiling docgen...."
-go install github.com/lmorg/murex/utils/docgen
+if [ "$MUREXVERSION" == "$OLDVER" ]; then
+    echo "No version change, skipping tests."
+else
+    #echo "Compiling stringer...."
+    #go build -o /bin/stringer golang.org/x/tools/cmd/stringer 
+    #
+    #echo "Updating auto-generated code...."
+    #go generate ./...
 
-echo "Compiling murex docs...."
-docgen -config gen/docgen.yaml
+    echo "Compiling docgen...."
+    go install github.com/lmorg/murex/utils/docgen
 
-echo "Compiling murex...."
-go install github.com/lmorg/murex
+    echo "Compiling murex docs...."
+    docgen -config gen/docgen.yaml
 
-echo "Running murex behavioural tests...."
-murex -c 'g: behavioural/* -> foreach: f { source $f }; try {test: run *}'
+    echo "Compiling murex...."
+    go install github.com/lmorg/murex
+
+    echo "Running murex behavioural tests...."
+    murex -c 'g: behavioural/* -> foreach: f { source $f }; try {test: run *}'
+fi
