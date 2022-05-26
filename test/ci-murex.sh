425ff9c315a856818304914365aed61fe9df23ee
Laurence Morgan <mail@laurencemorgan.co.uk>
2022-05-26 08:29:33 +0100

    Last few CSS tweaks

--- test/ci-murex.sh
+++ test/ci-murex.sh
@@ -4,7 +4,8 @@ set -ev
 
 . /etc/ci-murex.env
 
-export MUREXVERSION="$(murex -c 'version --no-app-name')"
+#export MUREXVERSION="$(murex -c 'version --no-app-name')"
+export MUREXVERSION="$(cat app/app.go | grep 'const Version' | egrep -o '[0-9]+\.[0-9]+\.[0-9]+')"
 OLDVER="$(curl -s https://murex.rocks/VERSION | head -n1)"
 
 if [ "$MUREXVERSION" == "$OLDVER" ]; then
