8b4e794091d8546da05970cb906afd74a57ddf37
Laurence Morgan <mail@laurencemorgan.co.uk>
2022-07-31 22:17:55 +0100

    #85 fix `f` builds on Windows and Plan9

--- builtins/core/io/globbing.go
+++ builtins/core/io/globbing.go
@@ -10,7 +10,6 @@ import (
 	"github.com/lmorg/murex/lang/types"
 	"github.com/lmorg/murex/utils/json"
 	"github.com/lmorg/murex/utils/lists"
-	"github.com/phayes/permbits"
 )
 
 func init() {
@@ -142,61 +141,8 @@ func cmdLsF(p *lang.Process) (err error) {
 		if err != nil {
 			continue
 		}
-		mode := info.Mode()
-		perm := permbits.FileMode(mode)
 
-		if ((add.File() && mode.IsRegular()) ||
-			(add.Dir() && mode.IsDir()) ||
-			(add.Symlink() && mode&os.ModeSymlink != 0) ||
-			(add.DevBlock() && mode&os.ModeDevice != 0) ||
-			(add.DevChar() && mode&os.ModeCharDevice != 0) ||
-			(add.Socket() && mode&os.ModeSocket != 0) ||
-			(add.NamedPipe() && mode&os.ModeNamedPipe != 0) ||
-
-			(add.UserRead() && perm.UserRead()) ||
-			(add.GroupRead() && perm.GroupRead()) ||
-			(add.OtherRead() && perm.OtherRead()) ||
-
-			(add.UserWrite() && perm.UserWrite()) ||
-			(add.GroupWrite() && perm.GroupWrite()) ||
-			(add.OtherWrite() && perm.OtherWrite()) ||
-
-			(add.UserExecute() && perm.UserExecute()) ||
-			(add.GroupExecute() && perm.GroupExecute()) ||
-			(add.OtherExecute() && perm.OtherExecute()) ||
-
-			(add.SetUid() && mode&os.ModeSetuid != 0) ||
-			(add.SetGid() && mode&os.ModeSetgid != 0) ||
-			(add.Sticky() && mode&os.ModeSticky != 0) ||
-
-			(add.Irregular() && mode&os.ModeIrregular != 0)) &&
-
-			!((remove.File() && mode.IsRegular()) ||
-				(remove.Dir() && mode.IsDir()) ||
-				(remove.Symlink() && mode&os.ModeSymlink != 0) ||
-				(remove.DevBlock() && mode&os.ModeDevice != 0) ||
-				(remove.DevChar() && mode&os.ModeCharDevice != 0) ||
-				(remove.Socket() && mode&os.ModeSocket != 0) ||
-				(remove.NamedPipe() && mode&os.ModeNamedPipe != 0) ||
-
-				(remove.UserRead() && perm.UserRead()) ||
-				(remove.GroupRead() && perm.GroupRead()) ||
-				(remove.OtherRead() && perm.OtherRead()) ||
-
-				(remove.UserWrite() && perm.UserWrite()) ||
-				(remove.GroupWrite() && perm.GroupWrite()) ||
-				(remove.OtherWrite() && perm.OtherWrite()) ||
-
-				(remove.UserExecute() && perm.UserExecute()) ||
-				(remove.GroupExecute() && perm.GroupExecute()) ||
-				(remove.OtherExecute() && perm.OtherExecute()) ||
-
-				(remove.SetUid() && mode&os.ModeSetuid != 0) ||
-				(remove.SetGid() && mode&os.ModeSetgid != 0) ||
-				(remove.Sticky() && mode&os.ModeSticky != 0) ||
-
-				(remove.Irregular() && mode&os.ModeIrregular != 0)) {
-
+		if matchFlags(add, remove, info) {
 			matched = append(matched, files[i])
 		}
 	}
---
+++ builtins/core/io/globbing_windows.go
@@ -0,0 +1,40 @@
+//go:build windows
+// +build windows
+
+package io
+
+import (
+	"os"
+)
+
+func matchFlags(add, remove fFlagsT, info os.FileInfo) bool {
+	mode := info.Mode()
+
+	return ((add.File() && mode.IsRegular()) ||
+		(add.Dir() && mode.IsDir()) ||
+		(add.Symlink() && mode&os.ModeSymlink != 0) ||
+		(add.DevBlock() && mode&os.ModeDevice != 0) ||
+		(add.DevChar() && mode&os.ModeCharDevice != 0) ||
+		(add.Socket() && mode&os.ModeSocket != 0) ||
+		(add.NamedPipe() && mode&os.ModeNamedPipe != 0) ||
+
+		(add.SetUid() && mode&os.ModeSetuid != 0) ||
+		(add.SetGid() && mode&os.ModeSetgid != 0) ||
+		(add.Sticky() && mode&os.ModeSticky != 0) ||
+
+		(add.Irregular() && mode&os.ModeIrregular != 0)) &&
+
+		!((remove.File() && mode.IsRegular()) ||
+			(remove.Dir() && mode.IsDir()) ||
+			(remove.Symlink() && mode&os.ModeSymlink != 0) ||
+			(remove.DevBlock() && mode&os.ModeDevice != 0) ||
+			(remove.DevChar() && mode&os.ModeCharDevice != 0) ||
+			(remove.Socket() && mode&os.ModeSocket != 0) ||
+			(remove.NamedPipe() && mode&os.ModeNamedPipe != 0) ||
+
+			(remove.SetUid() && mode&os.ModeSetuid != 0) ||
+			(remove.SetGid() && mode&os.ModeSetgid != 0) ||
+			(remove.Sticky() && mode&os.ModeSticky != 0) ||
+
+			(remove.Irregular() && mode&os.ModeIrregular != 0))
+}
