55e486aab279e6e1a27250bc70965b1f6ffefa1e
Laurence Morgan <mail@laurencemorgan.co.uk>
2022-11-02 14:26:20 +0000

    #479 egrep -> grep -E

--- test/ci-website.sh
+++ test/ci-website.sh
@@ -7,7 +7,7 @@ set -ev
 mkdir -p /website
 
 #export MUREXVERSION="$(murex -c 'version --no-app-name')"
-export MUREXVERSION="$(cat app/app.go | grep 'const Version' | egrep -o '[0-9]+\.[0-9]+\.[0-9]+')"
+export MUREXVERSION="$(cat app/app.go | grep 'const Version' | grep -E -o '[0-9]+\.[0-9]+\.[0-9]+')"
 OLDVER="$(curl -s https://murex.rocks/VERSION | head -n1)"
 
 if [ "$MUREXVERSION" == "$OLDVER" ]; then
