cc4bed77f64da642713ff46867dff5f1ac0cca36
Laurence Morgan <mail@laurencemorgan.co.uk>
2022-11-26 14:19:11 +0000

    Add marshellers for null

---
+++ builtins/types/null/marshal.go
@@ -0,0 +1,25 @@
+package null
+
+import (
+	"github.com/lmorg/murex/lang"
+	"github.com/lmorg/murex/lang/types"
+)
+
+func marshal(_ *lang.Process, v interface{}) ([]byte, error) {
+	i, err := types.ConvertGoType(v, types.Null)
+	if err != nil {
+		return types.FalseByte, err
+	}
+
+	s, err := types.ConvertGoType(i, types.String)
+	return []byte(s.(string)), err
+}
+
+func unmarshal(p *lang.Process) (interface{}, error) {
+	b, err := p.Stdin.ReadAll()
+	if err != nil {
+		return false, err
+	}
+
+	return types.ConvertGoType(b, types.Null)
+}
