7d6dd7b747235ff7198f57eb0a3d2782e7654cf6
Laurence Morgan <mail@laurencemorgan.co.uk>
2023-06-19 23:30:01 +0100

    #611 613 reverse out a ctrl+l bug fix as I believe it caused more issues

--- utils/readline/hotkey_functions.go
+++ utils/readline/hotkey_functions.go
@@ -33,9 +33,6 @@ func HkFnClearScreen(rl *Instance) {
 	print(seqSetCursorPosTopLeft + seqClearScreen)
 	rl.echo()
 	rl.renderHelpers()
-	if rl.line.RuneLen() == 0 {
-		moveCursorForwards(1)
-	}
 }
 
 func HkFnClearLine(rl *Instance) {
