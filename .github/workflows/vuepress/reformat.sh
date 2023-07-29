2ea55cd5ed56630ad0760c62771fc7f9448a259e
Laurence Morgan <mail@laurencemorgan.co.uk>
2023-07-29 12:08:32 +0100

    don't lowercase vuepress file names

--- .github/workflows/vuepress/reformat.sh
+++ .github/workflows/vuepress/reformat.sh
@@ -4,8 +4,8 @@ filename="$1"
 
 sed -r -i 's,<h2>Table of Contents</h2>,,' $filename
 
-lower="$(echo \"$filename\" | tr '[:upper:]' '[:lower:]')"
-if [ "$filename" = "$lower" ]; then
-    mv "$filename" "$lower"
-    filename="$lower"
-fi 
+#lower="$(echo \"$filename\" | tr '[:upper:]' '[:lower:]')"
+#if [ "$filename" = "$lower" ]; then
+#    mv "$filename" "$lower"
+#    filename="$lower"
+#fi 
