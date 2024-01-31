dbad74bff5ec0579704fce0710d7a4c3588605d1
Laurence Morgan <mail@laurencemorgan.co.uk>
2024-01-31 07:32:52 -0800

    #789 zero length string shouldn't panic when normalising path

--- utils/path_windows.go
+++ utils/path_windows.go
@@ -16,6 +16,11 @@ var rxPathPrefix *regexp.Regexp = regexp.MustCompile(`^[a-zA-Z]:[\\/]`)
 // NormalisePath takes a string and returns an absolute path if the string is a
 // relative path
 func NormalisePath(path string) string {
+	// bugfix: https://github.com/lmorg/murex/issues/789
+	if len(path) == 0 {
+		path = "."
+	}
+
 	pwd, err := os.Getwd()
 	if err == nil && path[0] != consts.PathSlash[0] && !rxPathPrefix.MatchString(path) {
 		path = pwd + consts.PathSlash + path
