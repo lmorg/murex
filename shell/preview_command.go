e37bd815859c0905971d0d3bfe139f998bbb33d0
Laurence Morgan <mail@laurencemorgan.co.uk>
2024-05-13 20:48:49 +0100

    check builtin is compiled before defaulting to doc in preview

--- shell/preview_command.go
+++ shell/preview_command.go
@@ -4,21 +4,20 @@ import (
 	"context"
 
 	"github.com/lmorg/murex/builtins/docs"
+	"github.com/lmorg/murex/builtins/events/onPreview/previewops"
 	"github.com/lmorg/murex/lang"
-	"github.com/lmorg/murex/lang/types"
 	"github.com/lmorg/murex/shell/autocomplete"
-	"github.com/lmorg/murex/utils/cache"
 	"github.com/lmorg/murex/utils/readline"
 )
 
-func PreviewCommand(ctx context.Context, _ []rune, command string, _ bool, size *readline.PreviewSizeT, callback readline.PreviewFuncCallbackT) {
+func PreviewCommand(ctx context.Context, cmdLine []rune, command string, _ bool, size *readline.PreviewSizeT, callback readline.PreviewFuncCallbackT) {
 	if lang.GlobalAliases.Exists(command) {
 		alias := lang.GlobalAliases.Get(command)
 		if len(alias) == 0 {
 			return
 		}
 		if alias[0] != command {
-			PreviewCommand(ctx, nil, alias[0], false, size, callback)
+			PreviewCommand(ctx, cmdLine, alias[0], false, size, callback)
 			return
 		}
 	}
@@ -28,15 +27,21 @@ func PreviewCommand(ctx context.Context, _ []rune, command string, _ bool, size 
 		if err != nil {
 			return
 		}
-		callback(previewParse([]byte(string(r)), size))
+		lines, _, err := previewParse([]byte(string(r)), size)
+		callback(lines, 0, err)
+		callEventsPreview(ctx, previewops.Function, command, cmdLine, lines, size, callback)
 		return
 	}
 
-	syn := docs.Synonym[command]
-	b := docs.Definition(syn)
-	if len(b) != 0 {
-		callback(previewParse(b, size))
-		return
+	if lang.GoFunctions[command] != nil {
+		syn := docs.Synonym[command]
+		b := docs.Definition(syn)
+		if len(b) != 0 {
+			lines, _, err := previewParse(b, size)
+			callback(lines, 0, err)
+			callEventsPreview(ctx, previewops.Builtin, command, cmdLine, lines, size, callback)
+			return
+		}
 	}
 
 	if !(*autocomplete.GlobalExes.Get())[command] {
@@ -44,7 +49,7 @@ func PreviewCommand(ctx context.Context, _ []rune, command string, _ bool, size 
 		return
 	}
 
-	b = manPage(command, size)
+	b := manPage(command, size)
 	var (
 		lines []string
 		err   error
@@ -52,20 +57,20 @@ func PreviewCommand(ctx context.Context, _ []rune, command string, _ bool, size 
 
 	if len(b) > 0 {
 		lines, _, err = previewParse(b, size)
-
 		callback(lines, 0, err)
-		lines = previewHr(lines, size)
 	}
 
-	block := []rune(`
+	callEventsPreview(ctx, previewops.Exec, command, cmdLine, lines, size, callback)
+
+	/*block := []rune(`
 		config set http user-agent curl/1.0
 		config set http timeout 2
 		trypipe {
 			get https://cheat.sh/$(COMMAND)?T -> [ Body ]
 		}`,
-	)
+	)*/
 
-	hash := cache.CreateHash(command, nil, block)
+	/*hash := cache.CreateHash(command, nil, block)
 	if !cache.Read(cache.PREVIEW_COMMAND, hash, &b) {
 
 		fork := lang.ShellProcess.Fork(lang.F_FUNCTION | lang.F_BACKGROUND | lang.F_NO_STDIN | lang.F_CREATE_STDOUT | lang.F_NO_STDERR)
@@ -90,10 +95,8 @@ func PreviewCommand(ctx context.Context, _ []rune, command string, _ bool, size 
 		}
 
 		cache.Write(cache.PREVIEW_COMMAND, hash, b, cache.Days(30))
-	}
+	}*/
 
-	s, _, err := previewParse(append(msgCheatSheet, b...), size)
-	callback(append(lines, s...), 0, err)
+	//s, _, err := previewParse(append(msgCheatSheet, b...), size)
+	//callback(append(lines, s...), 0, err)
 }
-
-var msgCheatSheet = []byte("The following is taken from https://cheat.sh:\n=============================================\n\n")
