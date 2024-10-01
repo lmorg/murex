adde27ea1643e193e2a3655653681e13f077b4b0
Laurence Morgan <mail@laurencemorgan.co.uk>
2024-10-01 23:21:54 +0100

    #867 better autocompletion support for `kill`

--- app/app.go
+++ app/app.go
@@ -16,9 +16,9 @@ const Name = "murex"
 const (
 	Major     = 6
 	Minor     = 4
-	Revision  = 283
+	Revision  = 287
 	Branch    = "develop"
-	BuildDate = "2024-10-01  22:15:56"
+	BuildDate = "2024-10-01  23:21:57"
 )
 
 // Copyright is the copyright owner string
