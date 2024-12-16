a6d3a58ddbcfe4bccc56f3679ec274da0df74f0b
Laurence Morgan <mail@laurencemorgan.co.uk>
2024-12-16 19:38:59 +0000

    csv: trim leading whitespace

--- app/app.go
+++ app/app.go
@@ -16,9 +16,9 @@ const Name = "murex"
 const (
 	Major     = 6
 	Minor     = 4
-	Revision  = 2007
+	Revision  = 2033
 	Branch    = "develop"
-	BuildDate = "2024-12-11  22:26:37"
+	BuildDate = "2024-12-16  19:39:03"
 )
 
 // Copyright is the copyright owner string
