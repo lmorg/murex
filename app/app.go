89c3f179d0ef697a00e7d0a3245eb3d11e4327f7
Laurence Morgan <mail@laurencemorgan.co.uk>
2025-04-15 09:22:10 +0100

    changelog: v7.0

--- app/app.go
+++ app/app.go
@@ -16,9 +16,9 @@ const Name = "murex"
 const (
 	Major     = 7
 	Minor     = 0
-	Revision  = 2029
+	Revision  = 2031
 	Branch    = "blog"
-	BuildDate = "2025-04-15  09:20:35"
+	BuildDate = "2025-04-15  09:22:15"
 )
 
 // Copyright is the copyright owner string
