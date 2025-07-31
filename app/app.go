6b409adc8c4b261df577345c467e236c75ec2dbf
Laurence Morgan <mail@laurencemorgan.co.uk>
2025-07-31 09:28:04 +0100

    website: `<code>` bg colour in dark mode

--- app/app.go
+++ app/app.go
@@ -16,9 +16,9 @@ const Name = "murex"
 const (
 	Major     = 7
 	Minor     = 1
-	Revision  = 1030
+	Revision  = 1031
 	Branch    = "develop"
-	BuildDate = "2025-07-31  09:20:05"
+	BuildDate = "2025-07-31  09:28:07"
 )
 
 // Copyright is the copyright owner string
