8708160ad538b9ea822b2ce7558d79e0564bfdc3
Laurence Morgan <mail@laurencemorgan.co.uk>
2025-08-14 12:28:37 +0100

    integrations: basti autocomplete

--- app/app.go
+++ app/app.go
@@ -16,9 +16,9 @@ const Name = "murex"
 const (
 	Major     = 7
 	Minor     = 1
-	Revision  = 2000
+	Revision  = 2007
 	Branch    = "develop"
-	BuildDate = "2025-08-01  23:56:11"
+	BuildDate = "2025-08-14  12:28:40"
 )
 
 // Copyright is the copyright owner string
