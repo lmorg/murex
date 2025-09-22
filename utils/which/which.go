e16d3adb4fbaabad0a25991476fc74419183cdb6
Laurence Morgan <mail@laurencemorgan.co.uk>
2025-09-22 10:11:15 +0100

    which: improve function description

--- utils/which/which.go
+++ utils/which/which.go
@@ -6,7 +6,8 @@ import (
 	"github.com/lmorg/murex/utils/consts"
 )
 
-// Which works similarly to the UNIX command with the same name
+// Which works similarly to the UNIX command with the same name.
+// If the executable is now found in $PATH then a zero length string is returned.
 func Which(cmd string) string {
 	_, err := os.Stat(cmd)
 	if !os.IsNotExist(err) {
@@ -26,6 +27,7 @@ func Which(cmd string) string {
 	return ""
 }
 
+// WhichIgnoreFail will always return a best guess of the executable
 func WhichIgnoreFail(cmd string) string {
 	path := Which(cmd)
 	if path == "" {
